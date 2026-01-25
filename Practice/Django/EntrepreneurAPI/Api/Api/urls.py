06f0e423668280f103698f09044c02cc382e6be1
Muhammad Mubeen <mubeeniqbal563@gmail.com>
2026-01-25 20:00:15 +0500

    Created url for router APIs

--- Practice/Django/EntrepreneurAPI/Api/Api/urls.py
+++ Practice/Django/EntrepreneurAPI/Api/Api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api-auth/', include('Apipractice.urls'))
+    path('api-auth/', include('Apipractice.urls')),
+    path("api/", include('Api.routers')),
 ]
