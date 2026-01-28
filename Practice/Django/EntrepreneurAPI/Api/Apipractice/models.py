d631da23655220139b7acf1a7247679f3c118cd5
Muhammad Mubeen <mubeeniqbal563@gmail.com>
2026-01-28 21:48:28 +0500

    Made Product model for API development

--- Practice/Django/EntrepreneurAPI/Api/Apipractice/models.py
+++ Practice/Django/EntrepreneurAPI/Api/Apipractice/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
@@ -38,3 +39,13 @@ class Manager(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+
+    @property
+    def sale_price(self):
+        return "%.2f" %(float(self.price) * 0.8)
