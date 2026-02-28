ed2dd9fd5e8fd7a05ab26e92de0f2a6980f331a5
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-02-28 13:00:58 +0500

    Hello module created

--- Practice/NestJS/n-fundamentals-pro/src/app.module.ts
+++ Practice/NestJS/n-fundamentals-pro/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [SongsModule],
+  imports: [SongsModule, HelloModule],
   controllers: [AppController],
   providers: [AppService],
 })
