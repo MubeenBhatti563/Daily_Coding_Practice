fa0e3a9988d567c5c3b0fe637164dd025cb74493
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-07 15:48:45 +0500

    exported helloService

--- Practice/NestJS/n-fundamentals-pro/src/hello/hello.module.ts
+++ Practice/NestJS/n-fundamentals-pro/src/hello/hello.module.ts
@@ -6,6 +6,6 @@ import { HelloService } from './hello.service';
   controllers: [HelloController],
   providers: [HelloService],
   // imports: [] import other module if needed
-  // exports: [] exports services if needed
+  exports: [HelloService], // exports services if needed
 })
 export class HelloModule {}
