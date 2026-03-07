78a68542df9e39594e4797028a859bc0498aacc9
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-07 15:47:55 +0500

    Added some modules

--- Practice/NestJS/n-fundamentals-pro/src/app.module.ts
+++ Practice/NestJS/n-fundamentals-pro/src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { HelloModule } from './hello/hello.module';
+import { UserModule } from './user/user.module';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [SongsModule, HelloModule],
+  imports: [
+    SongsModule, HelloModule, UserModule, TestModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
