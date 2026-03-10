4e267c9cef2516750b35751267d3c50aae266676
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-10 20:47:11 +0500

    Create a new posts module

--- Practice/NestJS/n-fundamentals-pro/src/app.module.ts
+++ Practice/NestJS/n-fundamentals-pro/src/app.module.ts
@@ -5,10 +5,11 @@ import { SongsModule } from './songs/songs.module';
 import { HelloModule } from './hello/hello.module';
 import { UserModule } from './user/user.module';
 import { TestModule } from './test/test.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
-    SongsModule, HelloModule, UserModule, TestModule
+    SongsModule, HelloModule, UserModule, TestModule, PostsModule
   ],
   controllers: [AppController],
   providers: [AppService],
