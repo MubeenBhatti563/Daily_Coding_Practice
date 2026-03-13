0ef10993d27a9492e95071bf0206cd4bd6995193
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-13 15:53:18 +0500

    Configured postgresql connectin

--- Practice/NestJS/n-fundamentals-pro/src/app.module.ts
+++ Practice/NestJS/n-fundamentals-pro/src/app.module.ts
@@ -6,10 +6,26 @@ import { HelloModule } from './hello/hello.module';
 import { UserModule } from './user/user.module';
 import { TestModule } from './test/test.module';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostEntity } from './posts/entities/post.entity';
 
 @Module({
   imports: [
-    SongsModule, HelloModule, UserModule, TestModule, PostsModule
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'ali123',
+      database: 'YoutubePosts',
+      entities: [PostEntity],
+      synchronize: true,
+    }),
+    SongsModule,
+    HelloModule,
+    UserModule,
+    TestModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
