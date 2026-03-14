a1084236514bd3fe2328d2d732ea0004cb507535
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-14 19:34:46 +0500

    Added TypeOrmModule for UserEntity

--- Practice/NestJS/n-fundamentals-pro/src/user/user.module.ts
+++ Practice/NestJS/n-fundamentals-pro/src/user/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { HelloModule } from '../hello/hello.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entitiy';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
