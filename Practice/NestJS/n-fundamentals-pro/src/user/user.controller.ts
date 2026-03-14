a2ba54ab5f8347fc0f86c796ce307b3da858a40a
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-14 19:33:33 +0500

    Created controller for User CRUD

--- Practice/NestJS/n-fundamentals-pro/src/user/user.controller.ts
+++ Practice/NestJS/n-fundamentals-pro/src/user/user.controller.ts
@@ -1,20 +1,59 @@
 import { UserService } from './user.service';
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
-import { HelloService } from 'src/hello/hello.service';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
+import { UserEntity } from './entities/user.entitiy';
+import { UserDto } from './dto/user.dto';
 
 @Controller('user')
+@UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get()
-  getNames() {
+  async getNames(): Promise<UserEntity[]> {
     return this.userService.getAllUsers();
   }
+
   @Get(':id')
-  getUserbyId(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.getUserbyId(id);
+  async getUserById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<UserEntity> {
+    return this.userService.getUserById(id);
   }
+
   @Get('hello/:id')
-  getUserGreet(@Param('id', ParseIntPipe) id: number): string {
+  async getUserGreet(@Param('id', ParseIntPipe) id: number): Promise<string> {
     return this.userService.getHelloName(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createUser(@Body() userData: UserDto): Promise<UserEntity> {
+    return this.userService.postUser(userData);
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() userData: UserDto,
+  ): Promise<UserEntity> {
+    return this.userService.updateUser(id, userData);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteUser(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    await this.userService.deleteUser(id);
   }
 }
