8f41b5c4862b6bf57ce7412993709ac36555834f
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-15 22:32:51 +0500

    Created Post Database

--- Practice/Django/Comments/comments/api/models.py
+++ Practice/Django/Comments/comments/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_post")
+
+    def __str__(self):
+        return f"{self.title} created by {self.user}"
+
+class Comment(models.Model):
+    text = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comment")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_comment")
+
+    def __str__(self):
+        return f"{self.text} of {self.post} by {self.user}"
