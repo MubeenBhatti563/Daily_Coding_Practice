3486b452e186ab8161485132918e80f266bc0da6
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-16 15:20:21 +0500

    Created PostViewSet

--- Practice/Django/Comments/comments/api/views.py
+++ Practice/Django/Comments/comments/api/views.py
@@ -1,6 +1,58 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .models import Post, Comment
+from .serializers import PostSerializer
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+class PostViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for Posts
+    """
+    queryset = Post.objects.all()
+
+    def list(self, request):
+        queryset = self.queryset
+        serializer = PostSerializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    def retrieve(self, request, pk=None):
+        try:
+            post = get_object_or_404(Post, pk=pk)
+            serializer = PostSerializer(post)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def update(self, request, pk=None):
+        try:
+            post = get_object_or_404(Post, pk=pk)
+            serializer = PostSerializer(post)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def destroy(self, request, pk=None):
+        try:
+            post = get_object_or_404(Post, pk=pk)
+            post.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def get_permissions(self):
+        if self.action == 'list':
+            self.permission_classes = [IsAuthenticatedOrReadOnly]
+        elif self.action == 'create':
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
