332859f4529df2761345fd06a8540c90b759b7fb
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-19 16:55:40 +0500

    UserEntity updated

--- Practice/NestJS/n-fundamentals-pro/src/user/entities/user.entitiy.ts
+++ Practice/NestJS/n-fundamentals-pro/src/user/entities/user.entitiy.ts
@@ -1,10 +1,18 @@
 import { Exclude } from 'class-transformer';
+import { PostEntity } from 'src/posts/entities/post.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
+
+export enum UserRole {
+  USER = 'user',
+  ADMIN = 'admin',
+}
 
 @Entity()
 export class UserEntity {
@@ -21,6 +29,19 @@ export class UserEntity {
   @Column()
   password: string;
 
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: UserRole;
+
+  @OneToMany(() => PostEntity, (post) => post.authorName)
+  posts: PostEntity[];
+
   @CreateDateColumn()
   createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
