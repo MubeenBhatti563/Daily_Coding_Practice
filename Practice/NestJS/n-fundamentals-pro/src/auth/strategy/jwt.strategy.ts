0a151017ca58d527ab3ba5be645e58d97be91423
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-20 19:47:06 +0500

    Strategy for authentication

---
+++ Practice/NestJS/n-fundamentals-pro/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'jwt_token',
+    });
+  }
+  async validate(payload: any) {
+    try {
+      const user = await this.authService.getUserById(payload.sub);
+      return {
+        ...user,
+        role: payload.role,
+      };
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+}
