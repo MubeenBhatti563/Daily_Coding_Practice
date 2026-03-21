d1ecd0f2e4a6d550ac45ad63317b9d80aac01d0b
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-21 15:29:53 +0500

    Configured auth module

--- Practice/NestJS/n-fundamentals-pro/src/auth/auth.module.ts
+++ Practice/NestJS/n-fundamentals-pro/src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/entities/user.entitiy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    PassportModule,
+    JwtModule.register({}),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
