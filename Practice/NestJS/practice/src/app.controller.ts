4d0144c2792bb4a3806f738f6c9ac9f6f859ba46
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-27 19:41:36 +0500

    Controller for Post and Get of products

--- Practice/NestJS/practice/src/app.controller.ts
+++ Practice/NestJS/practice/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { ProductEntity } from './entities/product.entity';
+import { ProductDto } from './dto/product.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @HttpCode(HttpStatus.ACCEPTED)
+  async getProducts(): Promise<ProductEntity[]> {
+    return this.appService.getProducts();
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.ACCEPTED)
+  async getProduct(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<ProductEntity | null> {
+    return this.appService.getProduct(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createProduct(@Body() productDto: ProductDto): Promise<ProductEntity> {
+    return this.appService.postProduct(productDto);
   }
 }
