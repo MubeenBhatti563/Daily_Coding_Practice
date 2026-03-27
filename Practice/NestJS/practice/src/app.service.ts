a21bbfd32419891a17357175cc6345bc5554d304
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-03-27 19:40:54 +0500

    Successfully create post and get method for products

--- Practice/NestJS/practice/src/app.service.ts
+++ Practice/NestJS/practice/src/app.service.ts
@@ -1,8 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ProductEntity } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductDto } from './dto/product.dto';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private productRepository: Repository<ProductEntity>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getProducts(): Promise<ProductEntity[]> {
+    return this.productRepository.find();
+  }
+
+  async getProduct(id: number): Promise<ProductEntity | null> {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product)
+      throw new NotFoundException(`Product with id ${id} is not found!`);
+    return product;
+  }
+
+  async postProduct(productDto: ProductDto): Promise<ProductEntity> {
+    const product = this.productRepository.create({
+      name: productDto.name,
+      quantity: productDto.quantity,
+      sales: productDto.sales,
+      price: productDto.price,
+      description: productDto.description,
+    });
+    return this.productRepository.save(product);
   }
 }
