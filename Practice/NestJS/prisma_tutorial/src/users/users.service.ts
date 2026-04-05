adf11ea3f1743ab48c48dbfac7d8b962ab3846ae
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-04-05 16:38:52 +0500

    User CRUD REST APIs

--- Practice/NestJS/prisma_tutorial/src/users/users.service.ts
+++ Practice/NestJS/prisma_tutorial/src/users/users.service.ts
@@ -1,6 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+import { UserDto } from './users.dto';
 
 @Injectable()
 export class UsersService {
-  constructor() {}
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUsers() {
+    return this.prisma.user.findMany();
+  }
+
+  async createUser(dto: UserDto) {
+    try {
+      return await this.prisma.user.create({
+        data: dto,
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException('Username or Email already exists');
+      }
+      throw error;
+    }
+  }
+
+  async getUserById(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  async updateUser(id: number, dto: UserDto) {
+    await this.getUserById(id);
+
+    try {
+      return await this.prisma.user.update({
+        where: { id },
+        data: dto,
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException('Username or Email already exists');
+      }
+      throw error;
+    }
+  }
+
+  async deleteUser(id: number) {
+    await this.getUserById(id);
+    await this.prisma.user.delete({ where: { id } });
+  }
 }
