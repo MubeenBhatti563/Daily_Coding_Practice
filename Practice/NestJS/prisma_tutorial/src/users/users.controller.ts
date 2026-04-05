91a6cd4180007c50f733eee064bd003f7fa882bb
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-04-05 16:38:28 +0500

    Implemented User CRUD

--- Practice/NestJS/prisma_tutorial/src/users/users.controller.ts
+++ Practice/NestJS/prisma_tutorial/src/users/users.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  ParseIntPipe,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './users.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createUser(@Body() userDto: UserDto) {
+    return this.userService.createUser(userDto);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getUserById(id);
+  }
+
+  @Patch(':id')
+  @HttpCode(HttpStatus.OK)
+  async updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() userDto: UserDto,
+  ) {
+    return this.userService.updateUser(id, userDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteUser(@Param('id', ParseIntPipe) id: number) {
+    await this.userService.deleteUser(id);
+  }
+}
