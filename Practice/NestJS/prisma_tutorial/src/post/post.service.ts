6c9bc1565dbf37ea0b53adae0b9498b1804edab9
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-04-17 15:55:04 +0500

    Created service for Get and Post groupPost

--- Practice/NestJS/prisma_tutorial/src/post/post.service.ts
+++ Practice/NestJS/prisma_tutorial/src/post/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class PostService {
@@ -11,6 +12,31 @@ export class PostService {
         title: data.title || '',
         description: data.description || '',
         userId,
+      },
+    });
+  }
+
+  createGroupPost(usersId: number[], data: Prisma.GroupPostCreateInput) {
+    return this.prisma.groupPost.create({
+      data: {
+        ...data,
+        users: {
+          create: usersId.map((id) => ({
+            userId: id,
+          })),
+        },
+      },
+    });
+  }
+
+  getGroupPost() {
+    return this.prisma.groupPost.findMany({
+      include: {
+        users: {
+          select: {
+            user: true,
+          },
+        },
       },
     });
   }
