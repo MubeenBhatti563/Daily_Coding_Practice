75638ae070accfc3b2438d039fa31025e35a3efd
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-04-17 15:52:40 +0500

    Implemented many-to-many relationship

--- Practice/NestJS/prisma_tutorial/src/post/post.controller.ts
+++ Practice/NestJS/prisma_tutorial/src/post/post.controller.ts
@@ -1,6 +1,7 @@
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateGroupPostDto } from './dto/create-group-post.dto';
 
 @Controller('post')
 export class PostController {
@@ -10,5 +11,16 @@ export class PostController {
   create(@Body() createPostDto: CreatePostDto) {
     const { userId, ...data } = createPostDto;
     return this.postService.createPost(Number(userId), data);
+  }
+
+  @Post('group')
+  createGroupPost(@Body() createGroupPostDto: CreateGroupPostDto) {
+    const { userId, ...data } = createGroupPostDto;
+    return this.postService.createGroupPost(userId.map(Number), data);
+  }
+
+  @Get('group')
+  getGroupPost() {
+    return this.postService.getGroupPost();
   }
 }
